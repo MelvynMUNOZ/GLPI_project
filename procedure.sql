f63c950f23c785578e82c554ede723f0279125b0
Math78650 <pereiramatheo78@gmail.com>
2024-03-18 15:35:01 +0100

    Priority + Operator_ID
    Ajout operator_ID dans la notif + Procédure et trigger qui gère la priority

--- procedure.sql
+++ procedure.sql
@@ -4,13 +4,49 @@
 -- Procédure pour notifier un utilisateur lorsqu'un ticket change de statut
 CREATE OR REPLACE PROCEDURE fn_notify_ticket_status_changed (
     p_user_id NUMBER,
+    p_operator_id NUMBER,
     p_ticket_id NUMBER,
     p_old_status VARCHAR2,
     p_new_status VARCHAR2
 ) AS
 BEGIN
-    INSERT INTO NOTIFICATIONS (USER_ID, TICKET_ID, MESSAGE, STATUS, DATE_CREATED)
-    VALUES (p_user_id, p_ticket_id, 'Statut changé de ' || p_old_status || ' à ' || p_new_status, p_new_status, SYSDATE);
+    INSERT INTO GLPI_NOTIFICATION (USER_ID, OPERATOR_ID,TICKET_ID, MESSAGE, STATUS, DATE_CREATED)
+    VALUES (p_user_id, p_operator_id,p_ticket_id, 'Statut changé de ' || p_old_status || ' à ' || p_new_status, p_new_status, SYSDATE);
 END;
 /
+
+ -- Procédure pour calculer la priorité d'un ticket
+CREATE OR REPLACE PROCEDURE fn_get_ticket_priority(
+    p_new_impact VARCHAR2,
+    p_new_urgency VARCHAR2,
+    p_priority OUT VARCHAR2
+) IS
+    v_impact_num NUMBER;
+    v_urgency_num NUMBER;
+    v_somme NUMBER;
+BEGIN
+    CASE p_new_impact
+        WHEN 'High' THEN v_impact_num := 3;
+        WHEN 'Medium' THEN v_impact_num := 2;
+        WHEN 'Low' THEN v_impact_num := 1;
+    END CASE;
+
+    CASE p_new_urgency
+        WHEN 'High' THEN v_urgency_num := 3;
+        WHEN 'Medium' THEN v_urgency_num := 2;
+        WHEN 'Low' THEN v_urgency_num := 1;
+    END CASE;
+
+    v_somme := v_impact_num + v_urgency_num;
+
+    IF v_somme >= 5 THEN
+        p_priority := 'High';
+    ELSIF v_somme = 4 THEN
+        p_priority := 'Medium';
+    ELSE
+        p_priority := 'Low';
+    END IF;
+END;
+/
+
 
--- trigger.sql
+++ trigger.sql
@@ -1,12 +1,24 @@
-
+--Ce fichier contient les triggers stockées pour la gestion des tickets
 ---------Trigger-------------
+ --Trigger pour l'envoie de notification lors du changement de statut
 CREATE OR REPLACE TRIGGER trig_notify_ticket_status_changed
 AFTER UPDATE OF STATUS ON GLPI_TICKET
 FOR EACH ROW
 BEGIN
      IF :NEW.STATUS <> :OLD.STATUS THEN -- Si le statut a changé
         -- Appelle la procédure fn_notify_ticket_status_changed avec l'ID de l'utilisateur
-        fn_notify_ticket_status_changed(:NEW.OWNER_ID, :NEW.ID, :OLD.STATUS, :NEW.STATUS);
+        fn_notify_ticket_status_changed(:NEW.OWNER_ID, :NEW.OPERATOR_ID,:NEW.ID, :OLD.STATUS, :NEW.STATUS);
     END IF;
+END;
+/
+ --Trigger pour la gestion des priorités
+CREATE OR REPLACE TRIGGER trig_ticket_set_priority
+BEFORE INSERT OR UPDATE OF IMPACT, URGENCY ON GLPI_TICKET
+FOR EACH ROW
+DECLARE
+    v_calculated_priority VARCHAR2(10); -- Variable pour stocker la priorité calculée
+BEGIN
+    fn_get_ticket_priority(:NEW.IMPACT, :NEW.URGENCY, v_calculated_priority); -- Appelle la procédure fn_get_ticket_priority pour calculer la priorité
+    :NEW.PRIORITY := v_calculated_priority; -- Affecte la priorité calculée à la priorité du ticket
 END;
 /
