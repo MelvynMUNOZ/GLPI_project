b79f18d11d46de8f7d47b54124891f3cfc125d0b
thomas-gons <gons.thomas@gmail.com>
2024-03-22 16:41:25 +0100

    bananana split ouhh

--- admin_view.sql
+++ admin_view.sql
@@ -1,5 +1,10 @@
+CREATE MATERIALIZED VIEW LOG ON view_admin_glpi@db_pau;
+CREATE MATERIALIZED VIEW LOG ON view_admin_glpi@db_cergy;
 
-CREATE MATERIALIZED VIEW view_admin_glpi AS
+
+CREATE MATERIALIZED VIEW view_admin_glpi
+    REFRESH FAST ON COMMIT
+    AS
     SELECT * FROM view_glpi_ticket@db_pau
         UNION
     SELECT * FROM view_glpi_ticket@db_cergy;
